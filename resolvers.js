1506bf63d48c15f7d8f296619bdab03d1ad6d092
Ryan DiMascio <ryan.dimascio@gmail.com>
2020-04-20 00:15:52 -0700

    Adds support for start and end date when getting stats in the product query

--- resolvers.js
+++ resolvers.js
@@ -2,46 +2,107 @@ const Product = require('./models/Product')
 const Stat = require('./models/Stat')
 
 const Resolvers = {
-  Query: {
-    Products: (_, { asin, category, stats = false, collection = "bestSeller", limit = 100, offset = 0 }, context) => {
-        const params = {}
+	Query: {
+		Products: (
+			_,
+			{
+				asin,
+				category,
+				stats = false,
+				start = null,
+				end = null,
+				collection = 'bestSeller',
+				sortBy = 'date',
+				order = 'desc',
+				limit = 100,
+				offset = 0,
+			},
+			context
+		) => {
+			const params = {}
+			const sort = {}
 
-        if (asin) {
-          params.asin = asin
-        }
+			sort[sortBy == 'date' ? '_id' : sortBy] =
+				order == 'desc' ? -1 : 1
 
-        if (category) {
-          params['category.primary'] = category
-        }
+			if (stats) {
+				const lookupQuery = {
+					from: `${collection}Stats`,
+					as: 'stats',
+				}
 
-        if (stats) {
-          return Product(collection).aggregate([
-              { $limit: limit },
-              { $skip: offset },
-              {
-                $lookup:
-                  {
-                    from: `${collection}Stats`,
-                    localField: 'asin',
-                    foreignField: 'asin',
-                    as: 'stats',
-                  }
-              }
-          ])
-        }
+				const query = [
+					{$sort: {...sort}},
+					{$limit: limit},
+					{$skip: offset},
+					{
+						$lookup: lookupQuery,
+					},
+				]
 
-        return Product(collection).find(params).limit(limit).skip(offset)
-    },
-    Stats: (_, { asin, collection = 'bestSeller', limit = 100, offset = 0 }, context) => {
-        const params = {}
-        
-        if (asin) {
-          params.asin = asin
-        }
+				if (asin) {
+					query.unshift({
+						$match: {asin: asin},
+					})
+				} else if (category) {
+					query.unshift({
+						$match: {'category.primary': category},
+					})
+				}
 
-        return Stat(collection).find(params).limit(limit).skip(offset)
-    }
-  }
-};
+				if (start && end) {
+					lookupQuery.let = {id: '$asin'}
+					lookupQuery.pipeline = [
+						{
+							$match: {
+								timestamp: {
+									$gte: new Date(start).toISOString(),
+									$lte: new Date(end).toISOString(),
+								},
+								$expr: {
+									$and: [
+										{
+											$eq: ['$asin', '$$id'],
+										},
+									],
+								},
+							},
+						},
+					]
+				} else {
+					lookupQuery.localField = 'asin'
+					lookupQuery.foreignField = 'asin'
+				}
+
+				return Product(collection).aggregate(query)
+			}
+
+			if (asin) {
+				params.asin = asin
+			} else if (category) {
+				params['category.primary'] = category
+			}
+
+			return Product(collection)
+				.find(params)
+				.sort(sort)
+				.limit(limit)
+				.skip(offset)
+		},
+		Stats: (
+			_,
+			{asin, collection = 'bestSeller', limit = 100, offset = 0},
+			context
+		) => {
+			const params = {}
+
+			if (asin) {
+				params.asin = asin
+			}
+
+			return Stat(collection).find(params).limit(limit).skip(offset)
+		},
+	},
+}
 
 module.exports = Resolvers
--- schema.js
+++ schema.js
@@ -1,42 +1,53 @@
-const { gql } = require('apollo-server-express')
+const {gql} = require('apollo-server-express')
 
 const Schema = gql`
-  type Category {
-    primary(name: String): String
-    secondary(name: String): String
-  }
-  type Stats {
-    timestamp: String
-    price: String
-    rank: Int
-    rating: String
-    reviews: Int
-    category: String
-  }
-  type Product {
-    asin: String
-    title: String
-    category: Category
-    image: String
-    price: String
-    rating: String
-    reviews: Int
-    rank: Int,
-    stats: [Stats!],
-  }
-  type Stat {
-	  asin: String
-	  category: String
-	  price: String
-	  rank: Int
-	  rating: String
-	  reviews: Int
-	  timestamp: String
-  }
-  type Query {
-	  Stats(asin: String, collection: String, limit: Int, offset: Int): [Stat]
-    Products(asin: String, category: String, collection: String, limit: Int, offset: Int, stats: Boolean): [Product]
-  }
-`;
+	type Category {
+		primary(name: String): String
+		secondary(name: String): String
+	}
+	type Stats {
+		timestamp(date: String): String
+		price: String
+		rank: Int
+		rating: String
+		reviews: Int
+		category: String
+	}
+	type Product {
+		asin: String
+		title: String
+		category: Category!
+		image: String
+		price: String
+		rating: String
+		reviews: Int
+		rank: Int
+		stats: [Stats!]
+	}
+	type Stat {
+		asin: String
+		category: String
+		price: String
+		rank: Int
+		rating: String
+		reviews: Int
+		timestamp: String
+	}
+	type Query {
+		Stats(asin: String, collection: String, limit: Int, offset: Int): [Stat]
+		Products(
+			asin: String
+			category: String
+			collection: String
+			sortBy: String
+			order: String
+			start: String
+			end: String
+			limit: Int
+			offset: Int
+			stats: Boolean
+		): [Product]
+	}
+`
 
 module.exports = Schema
